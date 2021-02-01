774981923559adb3781981812c300f2787b25137
Pavlova <approdv@gmail.com>
2021-02-01 20:45:08 +0300

    Added cod for physical device

--- The Light/ViewController.swift
+++ The Light/ViewController.swift
@@ -4,31 +4,47 @@
 //
 //  Created by Pavlova on 10.01.2021.
 //
-
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
     
     var isLightOn: Bool = true
     
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        isLightOn = !isLightOn
+        
+        updateView()
+        
+    }
+    
+    
+    
+    func updateView() {
+        let device = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        if let dev = device, dev.hasTorch {
+            view.backgroundColor = .black
+            do {
+                try dev.lockForConfiguration()
+                dev.torchMode = isLightOn ? .on : .off
+                dev.unlockForConfiguration()
+            } catch {
+                print (error)
+            }
+        } else {
+            view.backgroundColor = isLightOn ? .white : .black
+        }
+    }
+    
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateUI()
-        }
-
-    fileprivate func updateUI() {
-        view.backgroundColor = isLightOn ? .white : .black
+        updateView()
     }
-    
-    @IBAction func buttonPressed() {
-        isLightOn.toggle()
-        updateUI()
-    }
-    
 }
-
